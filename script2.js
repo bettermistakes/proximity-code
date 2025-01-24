b4e5ca14aab790c15b46ea52a9da89a220af4648
tarikjamil <tarikjamill@gmail.com>
2025-01-24 15:32:53 +0100

    Update script2.js

--- script2.js
+++ script2.js
@@ -148,7 +148,7 @@ function setupClickAndHold(onHoldComplete, holdDuration = 1000) {
 
         gsap.to(".svg--rotate", {
           scale: 1 + 0.4 * progress, // Scale from 1 to 1.4
-          rotate: 360 * progress, // Rotate from 0deg to 90deg
+          rotate: -360 * progress, // Rotate from 0deg to 90deg
           filter: `blur(${10 * progress}px)`, // Blur from 0px to 10px
           overwrite: true,
           duration: 0,
