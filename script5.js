a02b2a7544a821121889b75a8d064e95e32a25bd
tarikjamil <tarikjamill@gmail.com>
2025-02-06 18:12:31 +0100

    Update script5.js

--- script5.js
+++ script5.js
@@ -116,6 +116,17 @@ function pageLoad() {
     },
     "loadingAnimationsStart"
   );
+
+  // Start bouncing clue animation only after the page load finishes
+  tl.call(() => {
+    gsap.to(".svg--rotate", {
+      rotate: -5, // Rotate -10 degrees from current position
+      yoyo: true,
+      repeat: -1,
+      duration: 1,
+      ease: "power2.inOut",
+    });
+  });
 }
 
 pageLoad();
