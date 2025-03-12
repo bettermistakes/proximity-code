1a5756d23d357102d5df6576294ab2481de23d04
tarikjamil <tarikjamill@gmail.com>
2025-03-12 11:53:33 +0000

    Update home.js

--- home.js
+++ home.js
@@ -1,4 +1,8 @@
-gsap.set(".navbar", { color: "white", backgroundColor: "transparent" });
+gsap.set(".navbar", {
+  color: "white",
+  backgroundColor: "transparent",
+  opacity: 0,
+});
 
 function pageLoad() {
   let tl = gsap.timeline();
@@ -77,16 +81,15 @@ function pageLoad() {
     "loadingAnimationsStart+=4"
   );
 
-  tl.from(
+  tl.to(
     ".navbar",
     {
-      opacity: "0",
-      backgroundColor: "#eae9e4",
+      opacity: "1",
       y: "-20rem",
       ease: "smooth",
       duration: 0.6,
     },
-    "loadingAnimationsStart+=4"
+    "loadingAnimationsStart+=5"
   );
 
   tl.from(
@@ -97,7 +100,7 @@ function pageLoad() {
       ease: "smooth",
       duration: 0.6,
     },
-    "loadingAnimationsStart+=4"
+    "loadingAnimationsStart+=5"
   );
 
   // Start bouncing clue animation only after the page load finishes
@@ -167,6 +170,18 @@ document
     );
 
     clickTl.to(
+      ".navbar",
+      {
+        color: "black",
+        opacity: 1,
+        duration: 0.6,
+        ease: "power2.out",
+        stagger: staggerSettings,
+      },
+      "+=0.2"
+    );
+
+    clickTl.to(
       ".background--video",
       {
         opacity: 0,
@@ -205,14 +220,6 @@ document
       onComplete: () => {
         gsap.set(".section-old.is--home-6", { display: "none" });
       },
-    });
-
-    clickTl.to(".navbar", {
-      color: "black",
-      opacity: 1,
-      duration: 0.6,
-      ease: "power2.out",
-      stagger: staggerSettings,
     });
 
     const staggerSettings = {
